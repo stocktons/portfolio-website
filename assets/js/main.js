fa724ae4543be7bb961c616eba44676fe81a3689
stocktons <sarah@thanksforallthe.fish>
2021-09-23 11:53:24 -0400

    navbar and hero

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,33 @@
+"use strict"
 /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
 
+function linkAction() {
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(link => link.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
 
