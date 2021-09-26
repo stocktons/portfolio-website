6817ea815adf00024447779d2d3ca0d8455f49f7
stocktons <sarah@thanksforallthe.fish>
2021-09-26 00:08:11 -0400

    footer added, navigation JS started

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,7 +87,7 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
     },
 });
 
-/*==================== Review ====================*/
+/*==================== REVIEW ====================*/
 let swiperReview = new Swiper('.review__container', {
     loop: true,
     grabCursor: true,
@@ -104,8 +104,32 @@ let swiperReview = new Swiper('.review__container', {
     }
 });
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
 
+function linkAction() {
+    
+}
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        let sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(`.nav__menu a[href="${sectionId}"]`).classList.add('active-link');
+        } else {
+            document.querySelector(`.nav__menu a[href="${sectionId}"]`).classList.remove('active-link');
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive());
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 
