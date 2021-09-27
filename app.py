a5d113c416f2205a68ebc3e728482d26a040f068
stocktons <sarah@thanksforallthe.fish>
2021-09-27 18:14:07 -0400

    started to configure flask_mail

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
 from flask import Flask, render_template, request
 from forms import ContactForm
+from flask_mail import Mail, Message
+
+mail = Mail()
 
 app = Flask(__name__)
 app.secret_key = 'SEEKRITKEE'
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = ''
+app.config["MAIL_PASSWORD"] = ''
+
+mail.init_app(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_contact_form():
     """Handle the contact form."""
 
     form = ContactForm(request.form)
+    if request.method == 'GET':
+        return render_template('index.html', form=form)
 
     if request.method == 'POST' and form.validate():
-        return 'Form posted.'
+        msg = Message("Contact Form", 
+                      recipients="sarah@thanksforallthe.fish", 
+                      sender=(form.name.data, form.email.data))
+        msg.body = form.message.data
+        print(msg, "This is the message object")
+        mail.send(msg)
+        return render_template('index.html', success=True)
+ 
 
-    elif request.method == 'GET':
-        return render_template('index.html', form=form)
+    
 
--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
-
 from wtforms import StringField, TextAreaField, validators
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 
 
-class ContactForm(Form):
+
+class ContactForm(FlaskForm):
     """Form to accept name, email, and message."""
 
-    name = StringField("Name", [validators.Length(min=4, max=25)])
-    email = StringField("Email", [validators.Length(min=6, max=35)])
-    message = TextAreaField("Message", [validators.Length(max=250)])
+    name = StringField("Name", [validators.Required(), validators.Length(min=4, max=25)])
+    email = StringField("Email", [validators.Required(), validators.Length(min=6, max=35)])
+    message = TextAreaField("Message", [validators.Required(), validators.Length(max=250)])
