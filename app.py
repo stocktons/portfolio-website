4ab34091686d02da82c2e91a0b6fb3682bfcb115
stocktons <sarah@thanksforallthe.fish>
2021-09-27 21:36:56 -0400

    finished email functionality and error messages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from forms import ContactForm
 from flask_mail import Mail, Message
 
@@ -10,28 +10,36 @@ app.secret_key = 'SEEKRITKEE'
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = ''
+app.config["MAIL_USERNAME"] = 'sarah@thanksforallthe.fish'
 app.config["MAIL_PASSWORD"] = ''
 
 mail.init_app(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_contact_form():
     """Handle the contact form."""
 
     form = ContactForm(request.form)
-    if request.method == 'GET':
+
+    if request.method == 'POST':
+        if not form.validate():
+            flash('Oops! Something\'s not quite right with your submission.')
+            return render_template('index.html', form=form)
+        else:
+            """Send Email."""
+            msg = Message("Website Contact Form",
+                       recipients=["sarah@thanksforallthe.fish"],
+                       sender=(form.name.data, form.email.data))
+            msg.body = f"Name: {form.name.data} \n Email: {form.email.data} \n Message: {form.message.data}"
+            mail.send(msg)
+
+            """Reset form after sending email."""
+            form.name.data = ""
+            form.email.data = ""
+            form.message.data = ""
+
+            return render_template('index.html', form=form, success=True)
+
+    elif request.method == 'GET':
         return render_template('index.html', form=form)
-
-    if request.method == 'POST' and form.validate():
-        msg = Message("Contact Form", 
-                      recipients="sarah@thanksforallthe.fish", 
-                      sender=(form.name.data, form.email.data))
-        msg.body = form.message.data
-        print(msg, "This is the message object")
-        mail.send(msg)
-        return render_template('index.html', success=True)
- 
-
-    
-
--- forms.py
+++ forms.py
@@ -6,6 +6,6 @@ from flask_wtf import FlaskForm
 class ContactForm(FlaskForm):
     """Form to accept name, email, and message."""
 
-    name = StringField("Name", [validators.Required(), validators.Length(min=4, max=25)])
-    email = StringField("Email", [validators.Required(), validators.Length(min=6, max=35)])
-    message = TextAreaField("Message", [validators.Required(), validators.Length(max=250)])
+    name = StringField("Name", [validators.Required("Please enter your name.")])
+    email = StringField("Email", [validators.Required("Please enter your email."), validators.Length(min=6, max=35, message="Please enter a valid email address.")])
+    message = TextAreaField("Message", [validators.Required("Please enter a message."), validators.Length(max=250)])
