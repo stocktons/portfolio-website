752cd3205a00f5a4f46ab1f941417b92f63792b0
stocktons <sarah@thanksforallthe.fish>
2021-09-28 19:28:49 -0400

    made improvements to email validation

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from wtforms import StringField, TextAreaField, validators
 from flask_wtf import FlaskForm
+from wtforms.validators import Email 
 
 class ContactForm(FlaskForm):
     """Form to accept name, email, and message."""
 
-    name = StringField("Name", [validators.Required("Please enter your name.")])
-    email = StringField("Email", [validators.Required("Please enter your email."), validators.Length(min=6, max=35, message="Please enter a valid email address.")])
-    message = TextAreaField("Message", [validators.Required("Please enter a message."), validators.Length(max=250)])
+    name = StringField("Name", [validators.Required()])
+    email = StringField("Email", [Email(message="Not a valid email address."), validators.Required()])
+    message = TextAreaField("Message", [validators.Required(), validators.Length(max=250)])
