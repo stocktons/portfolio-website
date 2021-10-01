9711ad2a72905110b9df3b64803d83221c0bddf4
stocktons <sarah@thanksforallthe.fish>
2021-10-01 18:54:20 -0400

    different mail config

--- app.py
+++ app.py
@@ -10,9 +10,13 @@ app.secret_key = 'SEEKRITKEE'
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_SSL"] = True
+#app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = 'sarah@thanksforallthe.fish'
 app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PW")
+
+
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
 
 mail.init_app(app)
 
