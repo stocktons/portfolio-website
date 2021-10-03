83adf955051fdeea30a099281a5f274337198c28
stocktons <sarah@thanksforallthe.fish>
2021-10-03 15:57:57 -0400

    added flask-compress

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 from flask import Flask, render_template, request, flash
+from flask_compress import Compress
 from forms.forms import ContactForm
 from flask_mail import Mail, Message
 import os
 
-#mail = Mail()
-
 app = Flask(__name__)
+Compress(app)
+
 app.secret_key = 'SEEKRITKEE'
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
-#app.config["MAIL_PORT"] = 465
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
-#app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = 'sarah@thanksforallthe.fish'
 app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PW")
 
 mail = Mail(app)
-# mail.init_app(app)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_contact_form():
