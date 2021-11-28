1afbffccbffce15db0d4e6e6a0c6d37e572810cb
stocktons <sarah@thanksforallthe.fish>
2021-11-28 10:50:11 -0500

    added recaptcha to contact form

--- forms/forms.py
+++ forms/forms.py
@@ -1,5 +1,6 @@
+from flask_wtf.recaptcha.fields import RecaptchaField
 from wtforms import StringField, TextAreaField, validators
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms.validators import Email 
 
 class ContactForm(FlaskForm):
@@ -8,3 +9,4 @@ class ContactForm(FlaskForm):
     name = StringField("Name", [validators.Required()])
     email = StringField("Email", [Email(message="Not a valid email address."), validators.Required()])
     message = TextAreaField("Message", [validators.Required(), validators.Length(max=5000)])
+    recaptcha = RecaptchaField()
